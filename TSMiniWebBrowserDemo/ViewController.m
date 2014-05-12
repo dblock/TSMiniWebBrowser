3cdb8702d5190c60ab46d03d72964264f4256491
dblock <dblock@dblock.org>
2014-05-12 11:46:59 -0400

    Added modal browser snapshot test.

--- TSMiniWebBrowserDemo/ViewController.m
+++ TSMiniWebBrowserDemo/ViewController.m
@@ -28,50 +28,10 @@
 
 @implementation ViewController
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
-}
-
 #pragma mark - View lifecycle
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
-}
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
@@ -99,7 +59,7 @@
     
     webBrowser.modalDismissButtonTitle = @"Home";
     
-    [self presentModalViewController:webBrowser animated:YES];
+    [self presentViewController:webBrowser animated:YES completion:nil];
 }
 
 - (IBAction)openBrowserTabbarModeOne:(id)sender {
@@ -129,15 +89,12 @@
     
     UIViewController *fooController = [[UIViewController alloc] init];
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
-    
-    
+
     [tabBarController addChildViewController:webBrowser];
     [tabBarController addChildViewController:fooController];
     
     [self.navigationController pushViewController:tabBarController animated:YES];
 }
-
-
 
 #pragma mark - TSMiniWebBrowserDelegate
 
--- TSMiniWebBrowserDemoTests/ViewControllerTests.m
+++ TSMiniWebBrowserDemoTests/ViewControllerTests.m
@@ -10,10 +10,24 @@
 
 SpecBegin(ViewController)
 
-it(@"displays a browser", ^{
-    ViewController *vc = [[ViewController alloc] init];
+__block ViewController *vc;
+__block UIWindow *window;
+
+beforeEach(^{
+    window = [[UIWindow alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+    vc = [[ViewController alloc] init];
+    window.rootViewController = vc;
+    [window makeKeyAndVisible];
     expect(vc.view).willNot.beNil();
-    expect(vc.view).to.haveValidSnapshotNamed(@"default");
+});
+
+it(@"displays a list of options", ^{
+    expect(window).to.haveValidSnapshotNamed(@"default");
+});
+
+it(@"openBrowserModalMode", ^{
+    [vc openBrowserModalMode:nil];
+    expect(window).will.haveValidSnapshotNamed(@"openBrowserModalMode");
 });
 
 SpecEnd
